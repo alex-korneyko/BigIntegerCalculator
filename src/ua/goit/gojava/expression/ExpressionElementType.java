15428556c644bb96ff5a1552d9fc3db573805c49
alex-korneyko <a.korneyko@gmail.com>
2016-04-19 18:38:59 +0300

    Added 'ExpressionElementType' - parentheses. Added parameters in 'ParametrizedBigComputeTest' (parenthesis)

--- src/ua/goit/gojava/expression/ExpressionElementType.java
+++ src/ua/goit/gojava/expression/ExpressionElementType.java
@@ -5,7 +5,7 @@ package ua.goit.gojava.expression;
  */
 public enum ExpressionElementType {
 
-    BIG_INT, BIG_DECIMAL, BIG_FLOAT, PLUS, MINUS, MULTIPLY, DIVIDE, POWER;
+    BIG_INT, BIG_DECIMAL, BIG_FLOAT, PLUS, MINUS, MULTIPLY, DIVIDE, POWER, OPEN_PARENTHESIS, CLOSE_PARENTHESIS;
 
     @Override
     public String toString(){
@@ -20,6 +20,10 @@ public enum ExpressionElementType {
                 return "/";
             case POWER:
                 return "^";
+            case OPEN_PARENTHESIS:
+                return "(";
+            case CLOSE_PARENTHESIS:
+                return ")";
         }
 
         return null;
